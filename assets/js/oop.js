3fc3a7e5f85451056f1c0b25262202a8543f5760
vlaad <tuzhbir@gmail.com>
2026-08-23 22:20:31 +0300

    do OOP

--- assets/js/oop.js
+++ assets/js/oop.js
@@ -14,13 +14,50 @@ class foodProduct {
 				(1000 * 60 * 60 * 24),
 		);
 	}
-	getFullInfo() {}
+	getFullInfo() {
+		return ` ${this._title} ${this._category} ${this._brand} \nКількість днів від дати виготовлення: ${product.ageInDays}`;
+	}
 }
 
-let product = new foodProduct(
-	"HLIB",
-	"HLIB",
-	"ZHYTNII",
+const product = new foodProduct(
+	"Молоко",
+	"2,5%",
+	"Яготинське",
 	"2026-08-20",
 );
 console.log(product.ageInDays);
+console.log(product.getFullInfo());
+
+class DiscountedProduct extends foodProduct {
+	constructor(
+		title,
+		category,
+		brand,
+		manufactureDate,
+		maxDiscountLimit,
+	) {
+		super(title, category, brand, manufactureDate);
+		this._maxDiscountLimit = maxDiscountLimit;
+		this._currentDiscount = 0;
+	}
+	applyDiscount(percent) {
+		if (percent > this._maxDiscountLimit)
+			throw new Error(
+				`Знижка не може перевищувати допустимий ліміт: ${maxDiscountLimit}`,
+			);
+		this._currentDiscount = percent;
+		return true;
+	}
+	getFullInfo() {
+		return `${super.getFullInfo()}\n Поточна знижка: ${this._currentDiscount} \n Максимальний ліміт знижки: ${this._maxDiscountLimit}`;
+	}
+}
+
+const product2 = new DiscountedProduct(
+	"Кефір",
+	"1%",
+	"Молокія",
+	"2026-08-15",
+	50,
+);
+console.log(product2.getFullInfo());
